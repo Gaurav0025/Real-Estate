0e33f4503b2ac2a9e98a7689a816afbe8dfb99f1
Gaurav kumar <gkarora889@gmail.com>
2024-02-19 22:50:03 +0530

    add redux toolkit and redux persist

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {useState} from 'react';
 import {Link, useNavigate} from 'react-router-dom';
-
+import { signInStart,signInSuccess,signInFailure} from '../redux/user/userSlice.js';
+import {useDispatch, useSelector} from 'react-redux';
 
 export default function SignIn() {
   const [formData,setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading,error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange  = (e) => {
     setFormData({
       ...formData,
@@ -17,7 +18,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try{
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch('/backend/auth/signin',
     {
       method:'POST',
@@ -29,16 +30,13 @@ export default function SignIn() {
     const data = await res.json();
     console.log(data);
     if(data.success === false){
-      setLoading(false);
-      setError(data.message);
+      dispatch(signInFailure(data.message));
       return;
     }
-    setLoading(false);
-    setError(null);
+    dispatch(signInSuccess(data));
     navigate('/');
     }catch(error){
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message));
     }
     
   };
@@ -53,7 +51,7 @@ export default function SignIn() {
       <div className='flex gap-2 mt-5'>
         <p>Dont Have an account</p>
         <Link to={'/sign-up'}>
-          <span className='text-blue-700'>Sign in</span>
+          <span className='text-blue-700'>Sign Up</span>
         </Link>
         </div>
         {error && <p className='text-red-500 mt-5 '>{error}</p>}
