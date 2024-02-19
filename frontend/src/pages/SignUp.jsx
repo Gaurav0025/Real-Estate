bd3efbd7ca5f3c2b45066187cc1870db4a4483a6
Gaurav kumar <gkarora889@gmail.com>
2024-02-19 21:35:37 +0530

    so today we also complete sign in functionality

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -44,7 +44,7 @@ export default function SignUp() {
   };
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7 '>SignUp</h1>
+      <h1 className='text-3xl text-center font-semibold my-7 '>Sign Up</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input type="text" placeholder='username' className='border p-3 rounded-lg ' id='username' onChange={handleChange}/>
         <input type="email" placeholder='email' className='border p-3 rounded-lg ' id='email'onChange={handleChange}/>
